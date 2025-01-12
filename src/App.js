8ad0b279511b211405a55cea9980eb9762320bc7
Rahul Tak <rahultak955@gmail.com>
2025-01-12 22:22:50 +0530

    front end start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Navbar/>
       <h1>this is a website</h1>
-    </div>
+    </>
   );
 }
 
